cac7f560a45981762eae132e5fff6dbe6237e595
Rômulo Lima <37809622+RomuloLim@users.noreply.github.com>
2022-07-07 22:48:06 -0300

    Ajustes finais

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class EsquiProblem {
+public class Main {
     public static void main(String[] args) throws InterruptedException {
         Filas filas = new Filas();
 
@@ -20,19 +15,14 @@ public class EsquiProblem {
 
         while(countEsquiadores < 120){
             try {
-                //Cria um novo esquiador.
                 new Thread(new Esquiador(filas)).start();
-
-                //Aguarda um intervalo entre ]0, 1]s para criar
-                //um novo esquiador.
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
-                Logger.getLogger(EsquiProblem.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
 
             countEsquiadores++;
         }
-
 
         while (!reordena){
             if(filas.getLeftTriple().size() < 3 && filas.getRightTriple().size() < 3){
